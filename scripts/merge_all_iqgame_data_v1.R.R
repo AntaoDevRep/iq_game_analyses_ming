2d7ce38e75554653c1851b3b2db09fbd015153c9
AntaoDevRep <antaohappy1990@163.com>
2025-12-03 16:38:42 +0800

    add JMIR cohort
    find gaming data sets for JMIR cohort

--- scripts/merge_all_iqgame_data_v1.R.R
+++ scripts/merge_all_iqgame_data_v1.R.R
@@ -3,19 +3,14 @@
 ###### saved under the folder below: 
 datasets.folder <- "D:/SynologyDriveWork/local_work_ming/study - gamification/IQ-Game Test Data V2/completed datasets v2"
 
-workspace.root <- 'D:/SynologyDriveWork/R_workspace/gamification_study_all_analysis_R_v2'
+workspace.root <- 'D:/local_dev/iq_game_analyses_ming'
 local.workspace <- paste(workspace.root, "local_workspace", sep = "/")
 
 ## results of the validation script created by Istiyak 
-data.validation.save.path <- "D:\SynologyDriveWork\R_workspace\iq_game_analysis\data/new_validation_results"
+data.validation.save.path <- paste(workspace.root, "data", "new_validation_results.xlsx", sep = "/")
 
-JMIR.cohort.save.path <- 
-  
-
-
-
-
-
+JMIR.cohort.save.path <- paste(workspace.root, "data", "new_validation_results.xlsx", sep = "/")
+ECliniMed.cohort.save.path <- paste(workspace.root, "data", "eCliniMed cohort gaming data sets.xlsx", sep = "/")
 
 ## output folder
 output.folder <- paste(workspace.root, "outcomes_2024_06_17", "game_data_preprocessing_v3", sep = "/")
@@ -49,30 +44,39 @@ m.theme <- theme(text = element_text(size = m.font.size,  family="sans", color =
                  legend.position = "right")
 
 
-sub_data_set_folders <- list.files(datasets.folder, include.dirs = T) # get a list that contains names of all sub folders of the "datasets.folder"
-dataset.paths <- paste(datasets.folder, sub_data_set_folders, sep = "/")
+valida.df <- read.xlsx(file=data.validation.save.path, sheetIndex = 1)
 
-dataset.overview <- GET.IQGame.Dataset.Overview(datasets.folder, subfolder.included = T)
-head(dataset.overview)
-dataset.overview[dataset.overview$AC==""&dataset.overview$BFL==""&dataset.overview$BFR==""&dataset.overview$CP==""&dataset.overview$IJ=="", ]
+ECliniMed.sit.df <- read.xlsx(file=ECliniMed.cohort.save.path, sheetIndex = 1)
+ECliniMed.stand.df <- read.xlsx(file=ECliniMed.cohort.save.path, sheetIndex = 2)
 
+valida.df$ECliniMed.Seated <- ""
+valida.df[valida.df$testId_in_file %in% ECliniMed.sit.df$TestId, "ECliniMed.Seated"] <- "x"
+table(valida.df$ECliniMed.Seated)
 
-Extract.IQGame.Features(dataset.path=datasets.folder, subfolder.included=T, output.path=output.folder)
-
-
-
-
-
+valida.df$ECliniMed.Stand <- ""
+valida.df[valida.df$testId_in_file %in% ECliniMed.stand.df$TestId, "ECliniMed.Stand"] <- "x"
+table(valida.df$ECliniMed.Stand)
 
 
+## ------------ JMIR paper cohort ---------------------------------------------------
+JMIR.datasets.folder <- "D:/SynologyDriveWork/local_work_ming/study - gamification/IQ-Game Test Data/completed datasets"
+dataset.overview <- GET.IQGame.Dataset.Overview(JMIR.datasets.folder, subfolder.included = T)
+
+JMIR.pats.info.path <- "D:/local_dev/iq_game_analyses_ming/data/JMIR paper/DM MS Cohort 1 Summary n=329.xlsx"
+JMIR.pats.df <- read.xlsx(file=JMIR.pats.info.path, sheetIndex = 1)
+JMIR.pats.game.df <- subset(dataset.overview, ProbandId %in% JMIR.pats.df$ProbandId & SessionNo==1)
+JMIR.pats.df$ProbandId[!JMIR.pats.df$ProbandId %in% JMIR.pats.game.df$ProbandId]
 
 
-
-
-
-
-
-
+JMIR.subcohort2.pats.info.path <- "D:/local_dev/iq_game_analyses_ming/data/JMIR paper/DM MS Cohort 2 Summary n=173.xlsx"
+JMIR.subcohort2.pats.df  <- read.xlsx(file=JMIR.subcohort2.pats.info.path, sheetIndex = 1)
+JMIR.subcohort2.pats.game.df <- subset(dataset.overview, ProbandId %in% JMIR.subcohort2.pats.df$ProbandId & SessionNo==1)
+JMIR.subcohort2.pats.df$ProbandId[!JMIR.subcohort2.pats.df$ProbandId %in% JMIR.subcohort2.pats.game.df$ProbandId]
+ 
+JMIR.NCS.pats.info.path <- "D:/local_dev/iq_game_analyses_ming/data/JMIR paper/NCS Cohort Summary n=37.xlsx"
+JMIR.NCS.pats.df  <- read.xlsx(file=JMIR.NCS.pats.info.path, sheetIndex = 1)
+JMIR.NCS.pats.game.df <- subset(dataset.overview, ProbandId %in% JMIR.NCS.pats.df$ProbandId & SessionNo==1)
+JMIR.NCS.pats.df$ProbandId[!JMIR.NCS.pats.df$ProbandId %in% JMIR.NCS.pats.game.df$ProbandId]
 
 
 
